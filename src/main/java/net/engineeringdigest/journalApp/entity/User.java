5ed7f5a800d9b0962185eacf8b46722afc2a338b
Sajal Sangal <sajalsangal@gmail.com>
2024-10-22 15:05:43 +0530

    updated annotations for user and JournalEntry

--- src/main/java/net/engineeringdigest/journalApp/entity/User.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/User.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -17,6 +18,7 @@ public class User {
 
     @Id
     private ObjectId id;
+    @Indexed(unique = true)
     @NonNull
     private String userName;
     @NonNull
