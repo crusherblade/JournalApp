676cbe3699efbccc283c507873bac37597da73d5
Sajal Sangal <sajalsangal@gmail.com>
2024-10-23 17:01:05 +0530

    Created User controller, POJO , repository and service for user

---
+++ src/main/java/net/engineeringdigest/journalApp/controller/UserEntryController.java
@@ -0,0 +1,20 @@
+package net.engineeringdigest.journalApp.controller;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+public class UserEntryController {
+
+
+}
---
+++ src/main/java/net/engineeringdigest/journalApp/service/UserEntryService.java
@@ -0,0 +1,38 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.UserEntryRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class UserEntryService {
+
+    @Autowired
+    private UserEntryRepository userEntryRepository;
+
+    public void saveEntry(User userEntry)
+    {
+        userEntryRepository.save(userEntry);
+    }
+
+    public List<User> getAll()
+    {
+        return userEntryRepository.findAll();
+    }
+
+    public Optional<User> findById(ObjectId id)
+    {
+        return userEntryRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id)
+    {
+        userEntryRepository.deleteById(id);
+    }
+}
