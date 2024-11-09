6230ab3ff523f5ab876368e47c06e97808729d01
Sajal Sangal <sajalsangal@gmail.com>
2024-11-10 01:20:10 +0530

    Added admin authentication at role based

---
+++ src/main/java/net/engineeringdigest/journalApp/controller/AdminController.java
@@ -0,0 +1,31 @@
+package net.engineeringdigest.journalApp.controller;
+
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.service.UserEntryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    @Autowired
+    private UserEntryService userEntryService;
+
+    @GetMapping("/all-users")
+    public ResponseEntity<?> getAllUsers(){
+        List<User> all = userEntryService.getAll();
+        if(all != null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/create-admin-user")
+    public void createAdminUser(@RequestBody User user){
+        userEntryService.saveAdmin(user);
+    }
+}
